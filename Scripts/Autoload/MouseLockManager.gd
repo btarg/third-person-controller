c3c90e41112f998cae5504d1e223118194b66e3b
btarg <icrazyblaze1@gmail.com>
2024-08-27 23:05:05 +0100

    mouse lock fixed

--- Scripts/Autoload/MouseLockManager.gd
+++ Scripts/Autoload/MouseLockManager.gd
@@ -1,13 +1,19 @@
 extends Node3D
 
-var mouse_locked: bool = true
+var mouse_locked: bool = false
+
+func _ready() -> void:
+    toggle_mouse_lock()
 
 func _unhandled_input(event) -> void:
     if event is InputEventKey:
         if event.is_pressed() and event.keycode == KEY_ESCAPE:
-            if mouse_locked:
-                Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-                mouse_locked = false
-            else:
-                Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-                mouse_locked = true
+            toggle_mouse_lock()
+
+func toggle_mouse_lock() -> void:
+    if mouse_locked:
+        Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+        mouse_locked = false
+    else:
+        Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+        mouse_locked = true

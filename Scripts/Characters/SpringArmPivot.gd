bfd56ec8fb0ca8d8ee510a868623d0a3aae6c604
btarg <27726289+btarg@users.noreply.github.com>
2025-01-17 00:30:15 +0000

    add new curve for topdown camera (fixes a few camera bugs)

--- Scripts/Characters/SpringArmPivot.gd
+++ Scripts/Characters/SpringArmPivot.gd
@@ -5,7 +5,7 @@ enum CameraMode {
     TOP_DOWN,
     THIRD_PERSON,
 }
-## Third person camera mode is used for exploration, Top-down is the BG3 style camera
+
 @export var camera_mode: CameraMode = CameraMode.THIRD_PERSON
 
 @export_group("Rotation")
@@ -14,32 +14,34 @@ const LERP_VALUE: float = 0.15
 @export var max_sensitivity: float = 0.15
 @export var min_sensitivity: float = 0.075
 var current_sensitivity: float = min_sensitivity
-## Camera speed multiplier curve for when the stick is pushed fully in any direction.
+
+# Speed multiplier curve for right-stick input
 @export var camera_speed_curve: Curve
-## How far the camera can rotate up and down in degrees
+
+@export_group("Zoom")
+# Curve for top-down camera zoom angles
+@export var angle_zoom_curve: Curve
+# Use this value to smoothly sample the angle_zoom_curve (range: 0..1)
+var angle_lerp_value: float = 0.5
+## Determines how quickly we sample through the angle_zoom_curve
+@export var angle_lerp_value_offset := 0.03
+@export var zoom_speed: float = 0.25
+@export var scroll_angle_speed: float = 0.1
+
+@export_group("Spring arm")
 @export var spring_arm_clamp_degrees: float = 75.0
 var spring_arm_clamp := deg_to_rad(spring_arm_clamp_degrees)
-
 
 @export_group("Top-down camera")
 @export var top_down_min_angle_degrees: float = 10.0
 @export var top_down_max_angle_degrees: float = 75.0
-
-var min_angle_r := deg_to_rad(top_down_min_angle_degrees) # near horizontal is -min_angle_r
-var max_angle_r := deg_to_rad(top_down_max_angle_degrees) # very top-down is -max_angle_r
-
-## How fast the top-down camera can zoom when using the zoom input (right stick on controller)
-@export var zoom_speed: float = 0.25
-## How fast the angle changes when zooming in/out (typically higher = flatter)
-@export var scroll_angle_speed: float = 0.1
-
+var min_angle_r := deg_to_rad(top_down_min_angle_degrees)  # near horizontal is -min_angle_r
+var max_angle_r := deg_to_rad(top_down_max_angle_degrees)  # very top-down is -max_angle_r
 
 @export_group("Acceleration")
 @export var rotation_acceleration: float = 0.75
 @export var rotation_deceleration: float = 0.2
-## How fast we lerp through the curve
-@export var curve_acceleration_rate: float = 0.015
-## Modifier for how fast we lerp through the curve when moving horizontally (top down camera only)
+@export var horizontal_speed_curve_acceleration_rate: float = 0.015
 @export var top_down_horizontal_speed_modifier: float = 0.05
 
 @export_group("FOV")
@@ -50,13 +52,9 @@ var max_angle_r := deg_to_rad(top_down_max_angle_degrees) # very top-down is -ma
 const CAMERA_BLEND: float = 0.1
 
 @onready var spring_arm: SpringArm3D = $SpringArm3D
-# The camera is the first child of the spring arm
 @onready var camera: Camera3D = $SpringArm3D.get_child(0) as Camera3D
-
-# If we are the third person character, then our owner is the exploration player
 @onready var player := owner as CharacterBody3D
 
-# FIX: Enable by default to prevent desync that causes jittering on mouse
 @export var enabled: bool = true:
     get:
         return enabled
@@ -66,30 +64,23 @@ const CAMERA_BLEND: float = 0.1
             _setup_camera()
 
 @export_group("Freelook camera control")
-## Spring arm length used for zooming in and out
 @onready var target_spring_length := spring_arm.spring_length
 var min_spring_arm_length: float = 1.0
 var max_spring_arm_length: float = 12.0
 var spring_arm_scroll_speed: float = 1.0
 
 func _ready() -> void:
-    # Make the Third Person camera take priority
-    # TODO: Make this a setting in editor
-    if camera_mode == CameraMode.THIRD_PERSON:
+    if camera_mode == CameraMode.THIRD_PERSON and enabled:
         _setup_camera()
     
 func _setup_camera() -> void:
-    if enabled:
-        camera.make_current()
-        rotation_velocity_x = 0.0
-        rotation_velocity_y = 0.0
-        curve_lerp_value_tp = 0.0
-        curve_lerp_value_top_down = 0.0
-        spring_arm.rotation.x = clamp(spring_arm.rotation.x, -spring_arm_clamp, spring_arm_clamp)
+    camera.make_current()
+    rotation_velocity_x = 0.0
+    rotation_velocity_y = 0.0
+    curve_lerp_value_tp = 0.0
+    curve_lerp_value_top_down = 0.0
+    spring_arm.rotation.x = clamp(spring_arm.rotation.x, -spring_arm_clamp, spring_arm_clamp)
 
-# The controller helper doesn't check for mouse motion as this would be laggy,
-# so if we are using the mouse but it thinks we are using a controller, we need to correct it
-# we don't need to do this the other way around, as joystick input is always checked
 func _controller_helper_use_mouse() -> void:
     if ControllerHelper.is_using_controller:
         ControllerHelper.is_using_controller = false
@@ -99,7 +90,6 @@ func pivot_input_update(event: InputEvent) -> void:
     if not enabled:
         return
 
-    # Always capture mouse in third person, otherwise only when using the mouse
     if camera_mode == CameraMode.THIRD_PERSON:
         Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
     else:
@@ -108,7 +98,6 @@ func pivot_input_update(event: InputEvent) -> void:
         else:
             Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-    # Handle mouse input mode changes
     if event is InputEventMouseMotion and Input.mouse_mode != Input.MOUSE_MODE_VISIBLE:
         _controller_helper_use_mouse()
 
@@ -117,7 +106,6 @@ func pivot_input_update(event: InputEvent) -> void:
             rotate_y(-event.relative.x * 0.005)
             spring_arm.rotate_x(-event.relative.y * 0.005)
             spring_arm.rotation.x = clamp(spring_arm.rotation.x, -spring_arm_clamp, spring_arm_clamp)
-            # print(Vector2(spring_arm.rotation.x, spring_arm.rotation.y))
     else:
         if event is InputEventMouseMotion and Input.is_action_pressed("right_click"):
             rotate_y(-event.relative.x * 0.005)
@@ -125,35 +113,17 @@ func pivot_input_update(event: InputEvent) -> void:
             spring_arm.rotation.x = clamp(spring_arm.rotation.x, -spring_arm_clamp, spring_arm_clamp)
         elif event is InputEventMouseButton and event.is_pressed():
             if event.button_index == MOUSE_BUTTON_WHEEL_UP:
-                # Zoom in
                 target_spring_length = clamp(
                     spring_arm.spring_length - spring_arm_scroll_speed,
                     min_spring_arm_length,
                     max_spring_arm_length
                 )
-                # Flatten angle toward -min_angle_r
-                var new_angle := spring_arm.rotation.x + scroll_angle_speed
-                new_angle = clamp(new_angle, -max_angle_r, -min_angle_r)
-                
-                var angle_tween := get_tree().create_tween()
-                angle_tween.tween_property(spring_arm, "rotation:x", spring_arm.rotation.x, new_angle)
-                # spring_arm.rotation.x = new_angle
-
             elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
-                # Zoom out
                 target_spring_length = clamp(
                     spring_arm.spring_length + spring_arm_scroll_speed,
                     min_spring_arm_length,
                     max_spring_arm_length
                 )
-                # Look more down toward -max_angle_r
-                var new_angle := spring_arm.rotation.x - scroll_angle_speed
-                new_angle = clamp(new_angle, -max_angle_r, -min_angle_r)
-
-                var angle_tween := get_tree().create_tween()
-                angle_tween.tween_property(spring_arm, "rotation:x", spring_arm.rotation.x, new_angle)
-
-                # spring_arm.rotation.x = new_angle
 
 func camera_physics_process(_delta) -> void:
     if not enabled:
@@ -161,30 +131,23 @@ func camera_physics_process(_delta) -> void:
 
     if change_fov_on_run and camera_mode == CameraMode.THIRD_PERSON:
         if player.is_on_floor() and player.velocity.length() > 0:
-            if player.is_running:
-                camera.fov = lerp(camera.fov, run_fov, CAMERA_BLEND)
-            else:
-                camera.fov = lerp(camera.fov, normal_fov, CAMERA_BLEND)
+            camera.fov = lerp(camera.fov, run_fov if player.is_running else normal_fov, CAMERA_BLEND)
         else:
             camera.fov = lerp(camera.fov, normal_fov, CAMERA_BLEND)
 
-    # Lerp the spring length
+    # Smoothly lerp spring_arm length
     spring_arm.spring_length = lerp(spring_arm.spring_length, target_spring_length, LERP_VALUE)
 
     if ControllerHelper.is_using_controller:
         _handle_controller_input()
 
 
-# These variables are not declared inside _handle_controller_input because
-# they would be reset every frame
 var rotation_velocity_y: float = 0.0
 var rotation_velocity_x: float = 0.0
-# Current value for sampling the curve
 var curve_lerp_value_top_down: float = 0.0
 var curve_lerp_value_tp: float = 0.0
 
 func _handle_controller_input() -> void:
-
     var right_stick := Vector2(
         Input.get_action_strength("look_left") - Input.get_action_strength("look_right"),
         Input.get_action_strength("look_up") - Input.get_action_strength("look_down")
@@ -193,67 +156,51 @@ func _handle_controller_input() -> void:
     if camera_mode == CameraMode.TOP_DOWN:
         # ---------------------------------------------------------
         # Baldur's Gate 3–style top-down camera
-        # Up on stick => zoom in + flatten angle
-        # Down on stick => zoom out + look down more
         # ---------------------------------------------------------
-
-        # Use the speed curve for horizontal rotation:
-        # The exploration camera's curve accelerates way too fast for the top-down camera,
-        # so I'm being lazy and just multiplying the curve sample value by a small number rather
-        # than making a separate curve for it. - 14/01/2025
         if (abs(right_stick.x) + abs(right_stick.y)) >= 1:
-            curve_lerp_value_top_down = lerp(curve_lerp_value_top_down, 1.0, curve_acceleration_rate * top_down_horizontal_speed_modifier)
+            curve_lerp_value_top_down = lerp(curve_lerp_value_top_down, 1.0, horizontal_speed_curve_acceleration_rate * top_down_horizontal_speed_modifier)
         else:
             curve_lerp_value_top_down = 0.0
 
         var curve_value := camera_speed_curve.sample(curve_lerp_value_top_down)
-
         current_sensitivity = min_sensitivity + (max_sensitivity - min_sensitivity) * curve_value
 
-        # Treat Y > 0 as zooming in (flatten angle) and Y < 0 as zooming out (more top-down)
-        if right_stick.y > 0.1:
-            # Zoom in
+        # Increase or decrease the angle_lerp_value based on Y
+        if right_stick.y > 0.1:  # Zoom in
             target_spring_length = clamp(
                 target_spring_length - zoom_speed,
                 min_spring_arm_length,
                 max_spring_arm_length
             )
-            # Flatten angle toward -min_angle_r
-            var new_angle := spring_arm.rotation.x + scroll_angle_speed
-            new_angle = clamp(new_angle, -max_angle_r, -min_angle_r)
-            rotation_velocity_x += new_angle
-
-        elif right_stick.y < -0.1:
-            # Zoom out
+            angle_lerp_value = clamp(angle_lerp_value - angle_lerp_value_offset, 0.0, 1.0)
+        elif right_stick.y < -0.1:  # Zoom out
             target_spring_length = clamp(
                 target_spring_length + zoom_speed,
                 min_spring_arm_length,
                 max_spring_arm_length
             )
-            # Look down more toward -max_angle_r
-            var new_angle := spring_arm.rotation.x - scroll_angle_speed
-            new_angle = clamp(new_angle, -max_angle_r, -min_angle_r)
-            rotation_velocity_x += new_angle
+            angle_lerp_value = clamp(angle_lerp_value + angle_lerp_value_offset, 0.0, 1.0)
 
-        # Horizontal rotation (X axis of stick)
+        # Sample our angle_zoom_curve (range: 0..1)
+        var normalized_sample := angle_zoom_curve.sample(angle_lerp_value)
+        # Convert the curve’s [0..1] range to angles [-max_angle_r..-min_angle_r]
+        var final_angle : float = lerp(-max_angle_r, -min_angle_r, normalized_sample)
+        spring_arm.rotation.x = final_angle
+
+        # Horizontal rotation
         right_stick.x *= current_sensitivity
-
-        # clamp angles for top-down
-        spring_arm.rotation.x = clamp(spring_arm.rotation.x, -max_angle_r, -min_angle_r)
 
     else:
         # ---------------------------------------------------------
         # Persona-style third-person "exploration" camera
         # ---------------------------------------------------------
-
         if (abs(right_stick.x) + abs(right_stick.y)) >= 1:
-            curve_lerp_value_tp = lerp(curve_lerp_value_tp, 1.0, curve_acceleration_rate)
+            curve_lerp_value_tp = lerp(curve_lerp_value_tp, 1.0, horizontal_speed_curve_acceleration_rate)
         else:
             curve_lerp_value_tp = 0.0
 
         var curve_value_tp := camera_speed_curve.sample(curve_lerp_value_tp)
         current_sensitivity = min_sensitivity + (max_sensitivity - min_sensitivity) * curve_value_tp
-
         right_stick *= current_sensitivity
 
     rotation_velocity_x = lerp(
@@ -270,11 +217,11 @@ func _handle_controller_input() -> void:
     rotation_velocity_x = clamp(rotation_velocity_x, -current_sensitivity, current_sensitivity)
     rotation_velocity_y = clamp(rotation_velocity_y, -current_sensitivity, current_sensitivity)
 
-    # APPLY TO BOTH CAMERA MODES
+    # Apply to both camera modes
     if abs(rotation_velocity_y) > 0.001 or abs(rotation_velocity_x) > 0.001:
         rotate_y(rotation_velocity_y)
-        spring_arm.rotate_x(rotation_velocity_x)
-        spring_arm.rotation.x = clamp(spring_arm.rotation.x, -spring_arm_clamp, spring_arm_clamp)
-
-    # on the spring arm, rotation X is up/down and rotation Y is left/right
-    # print(Vector2(spring_arm.rotation.x, spring_arm.rotation.y))
+        # For TOP_DOWN mode, X rotation is also coming from angle_zoom_curve, 
+        # but the minimal stick input can still nudge the angle slightly:
+        if camera_mode == CameraMode.THIRD_PERSON:
+            spring_arm.rotate_x(rotation_velocity_x)
+            spring_arm.rotation.x = clamp(spring_arm.rotation.x, -spring_arm_clamp, spring_arm_clamp)
